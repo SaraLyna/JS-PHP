358d2eaab585481ac0ab7feebe78f6e4d75d9884
Ouyahia Sara-lyna <saralyna.ouyahia.etu@a14p16.fil.univ-lille.fr>
2022-12-07 13:58:14 +0100

    td5

---
+++ td5/lib/DataLayer.class.php
@@ -0,0 +1,41 @@
+<?php
+class DataLayer {
+	// private ?PDO $connexion = NULL; // le typage des attributs est valide uniquement pour PHP>=7.4
+
+	private  $connexion = NULL; // connexion de type PDO   compat PHP<=7.3
+	
+	/**
+	 * @param $DSNFileName : file containing DSN 
+	 */
+	function __construct(string $DSNFileName){
+		$dsn = "uri:$DSNFileName";
+		$this->connexion = new PDO($dsn);
+		// paramètres de fonctionnement de PDO :
+		$this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // déclenchement d'exception en cas d'erreur
+		$this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC); // fetch renvoie une table associative
+		// réglage d'un schéma par défaut :
+		$this->connexion->query('set search_path=authent');
+	}
+    
+    
+    function authentificationProvisoire(string $login, string $password) : ?Identite{
+       // à compléter
+    }
+    
+    function authentification(string $login, string $password) : ?Identite{ // version password hash
+            // à compléter
+    }
+    /**
+    * @return bool indiquant si l'ajout a été réalisé
+    */
+    function createUser(string $login, string $password, string $nom, string $prenom) : bool	 {
+        $sql = <<<EOD
+        insert into "users" (login, password, nom, prenom)
+        values (:login, :password, :nom, :prenom)
+EOD;
+               // à compléter
+
+    }
+	
+}
+?>
